9c67b0b76de298ad8e615610a31ddee3bc1c38b6
Auzan Muhammad <eaglenest64@gmail.com>
2016-05-23 22:34:03 +0700

    Delete _db_init.py

--- _db_init.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-class vehicleDB:
-    def __init__(self, sql_file):
-        conn = sqlite3.connect(sql_file)
-        self.c = conn.cursor()
-
-    def createTable(self):
-        print "test"
